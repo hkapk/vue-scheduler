2af7bc4852e82ddb0567b12a6a3df33333478057
Hank Kapka <hankkapka@gmail.com>
2022-03-27 18:54:28 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <h1> ADD WEEKLY LESSON </h1>
+  <pre> {{ data }} </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      q: ''
+    }
+  },
+  async fetch () {
+    this.data = await this.$content('data')
+    .only(['date', 'times'])
+    .fetch()
+  }
+}
+</script>
